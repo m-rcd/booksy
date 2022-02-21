2e609d0ee6072bd6105c33782c2b04ebb6e567a4
Marianne Rachid <marianne.rachid@gmail.com>
2022-02-21 15:45:45 +0000

    Use mysql database

--- main.go
+++ main.go
@@ -1,11 +1,15 @@
 package main
 
 import (
+	"database/sql"
 	"encoding/json"
 	"fmt"
 	"io/ioutil"
 	"log"
 	"net/http"
+	"os"
+
+	_ "github.com/go-sql-driver/mysql"
 
 	"github.com/gorilla/mux"
 )
@@ -20,7 +24,7 @@ func handleRequests() {
 	myRouter.HandleFunc("/", homePage)
 	myRouter.HandleFunc("/books", returnAllBooks)
 	myRouter.HandleFunc("/book/{id}", updateBook).Methods("PATCH")
-	myRouter.HandleFunc("book/", createNewBook).Methods("POST")
+	myRouter.HandleFunc("/book", createNewBook).Methods("POST")
 	myRouter.HandleFunc("/book/{id}", deleteBook).Methods("DELETE")
 	myRouter.HandleFunc("/book/{id}", returnSingleBook)
 
@@ -34,18 +38,43 @@ type Book struct {
 	Content string `json:"content"`
 }
 
-var Books []Book
+var (
+	Books []Book
+	db    *sql.DB
+	err   error
+)
 
 func main() {
-	fmt.Println("Rest API v2.0 - Mux Routers")
-	Books = []Book{
-		{ID: "1", Title: "pantalaimon", Author: "Casper", Content: "hola"},
-		{ID: "2", Title: "SillyCanoofy", Author: "Jasper", Content: "I am silly"},
+	fmt.Println("Listening on port 10000")
+	dbUsername := os.Getenv("DB_USERNAME")
+	dbPassword := os.Getenv("DB_PASSWORD")
+	connection := fmt.Sprintf("%s:%s@tcp(127.0.0.1:3306)/books", dbUsername, dbPassword)
+	db, err = sql.Open("mysql", connection)
+	if err != nil {
+		panic(err.Error())
 	}
+
+	defer db.Close()
+
 	handleRequests()
 }
 
 func returnAllBooks(w http.ResponseWriter, r *http.Request) {
+	w.Header().Set("Content-Type", "application/json")
+	result, err := db.Query("SELECT * from books")
+	if err != nil {
+		panic(err.Error())
+	}
+	defer result.Close()
+
+	for result.Next() {
+		var book Book
+		err := result.Scan(&book.ID, &book.Title, &book.Author, &book.Content)
+		if err != nil {
+			panic(err.Error())
+		}
+		Books = append(Books, book)
+	}
 	fmt.Println("Endpoint Hit: returnAllBooks")
 	json.NewEncoder(w).Encode(Books)
 }
@@ -53,46 +82,50 @@ func returnAllBooks(w http.ResponseWriter, r *http.Request) {
 func returnSingleBook(w http.ResponseWriter, r *http.Request) {
 	id := mux.Vars(r)["id"]
 
-	for _, book := range Books {
-		if book.ID == id {
-			json.NewEncoder(w).Encode(book)
-		}
+	var book Book
+	w.Header().Set("Content-Type", "application/json")
+	result := db.QueryRow("SELECT * FROM books WHERE id = ?", id)
+
+	err := result.Scan(&book.ID, &book.Title, &book.Author, &book.Content)
+	if err != nil {
+		panic(err.Error())
 	}
+	json.NewEncoder(w).Encode(book)
 }
 
 func createNewBook(w http.ResponseWriter, r *http.Request) {
-	reqBody, _ := ioutil.ReadAll(r.Body)
-	var book Book
-	json.Unmarshal(reqBody, &book)
-	Books = append(Books, book)
+	stmt, err := db.Prepare("INSERT INTO books(title, author, content) VALUES(?, ?, ?)")
+	if err != nil {
+		panic(err.Error())
+	}
 
-	json.NewEncoder(w).Encode(book)
+	reqBody, _ := ioutil.ReadAll(r.Body)
+	var newBook Book
+	json.Unmarshal(reqBody, &newBook)
+
+	_, err = stmt.Exec(newBook.Title, newBook.Author, newBook.Content)
+	if err != nil {
+		panic(err.Error())
+	}
+
+	fmt.Fprintf(w, "New Book was created")
 }
 
 func deleteBook(w http.ResponseWriter, r *http.Request) {
 	id := mux.Vars(r)["id"]
-
-	for index, book := range Books {
-		if book.ID == id {
-			Books = append(Books[:index], Books[index+1:]...)
-		}
+	_, err := db.Query("DELETE FROM books WHERE id = ?", id)
+	if err != nil {
+		panic(err.Error())
 	}
+	fmt.Fprintf(w, "Book was deleted")
 }
 
 func updateBook(w http.ResponseWriter, r *http.Request) {
 	id := mux.Vars(r)["id"]
-
 	reqBody, _ := ioutil.ReadAll(r.Body)
 	var book Book
 	json.Unmarshal(reqBody, &book)
 
-	for i, b := range Books {
-		if b.ID == id {
-			b.Author = book.Author
-			b.Content = book.Content
-			b.Title = book.Title
-			Books = append(Books[:i], b)
-			json.NewEncoder(w).Encode(b)
-		}
-	}
+	db.Exec("UPDATE books set author=?, title=?, content=? where id=?", book.Title, book.Author, book.Content, id)
+
 }
